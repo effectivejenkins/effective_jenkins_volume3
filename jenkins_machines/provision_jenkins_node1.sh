bb1c32149c32b4796354c49d514cd0f07b27eb75
Rodrigo Russo <rodrigozrusso@gmail.com>
2018-02-25 17:51:34 +0100

    Fix ssh key on machines and add app_server

---
+++ jenkins_machines/provision_jenkins_node1.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+JENKINS_USER_HOME=/home/jenkins/
+
+# add username
+sudo adduser --disabled-password --gecos "" jenkins
+# Add the jenkins to sudo group
+sudo usermod -aG sudo jenkins
+#Add a group called docker
+sudo groupadd docker
+#Add the jenkins user to this group
+sudo usermod -aG docker jenkins
+
+# set password
+echo "jenkins:jenkins" | chpasswd
+
+###### Docker installation
+# remove old instalation of docker
+sudo apt-get remove -y docker docker-engine docker-ce docker.io
+
+# Add docker GPG Key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install -y default-jdk
+sudo apt-get install -y\
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
+
+sudo apt-get install -y\
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+sudo apt-get install -y docker-ce
+
+# Add Machines to known_hosts
+ssh-keyscan -H 192.168.50.6 >> ${JENKINS_USER_HOME}/.ssh/known_hosts
+
+sudo chown -R jenkins.jenkins ${JENKINS_USER_HOME}
+sudo chmod 600 ${JENKINS_USER_HOME}/.ssh/authorized_keys
+
+sudo reboot
